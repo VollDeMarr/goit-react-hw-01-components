53618fb61a769ee750065297511847282493872d
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2022-07-01 15:53:08 +0300

    4/4 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import Transactions from './Transactions/Transactions';
 
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
+import transactions from '../transactions.json';
 
 export default function App() {
   return (
@@ -30,22 +32,8 @@ export default function App() {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
+
+      <Transactions items={transactions}/>
     </div>
   );
 }
-// export const App = () => {
-//     return (
-//       <div
-//         style={{
-//           height: '100vh',
-//           display: 'flex',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//           fontSize: 40,
-//           color: '#010101'
-//         }}
-//       >
-//         React homework template
-//       </div>
-//     );
-//   };
---
+++ src/components/Transactions/Transactions.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+export default function Transactions({ items }) {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+Transactions.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            currency: PropTypes.string,
+          amount: PropTypes.string,
+          type: PropTypes.string,
+          id: PropTypes.string,
+        })
+      ),
+}
